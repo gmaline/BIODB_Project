fc0d12255c25cd244432a5960a7e0fd9066038c7
gmaline <66379163+gmaline@users.noreply.github.com>
2021-04-18 15:29:04 -0500

    GO Enrichment up and Running!

--- GO.py
+++ GO.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import csv
 
 import goatools
@@ -8,60 +9,72 @@ from goatools.base import download_go_basic_obo
 import genes_NCBI_9606_ProteinCoding
 import KEGG_Pathways
 
-GeneID2nt_hum = genes_NCBI_9606_ProteinCoding.GENEID2NT
-
-obo_fname = download_go_basic_obo()
-
-fin_gene2go = download_ncbi_associations()
-
-obodag = GODag("go-basic.obo")
-
-# Read NCBI's gene2go. Store annotations in a list of namedtuples
-objanno = Gene2GoReader(fin_gene2go, taxids=[9606])
-
-# Get namespace2association where:
-#    namespace is:
-#        BP: biological_process
-#        MF: molecular_function
-#        CC: cellular_component
-#    assocation is a dict:
-#        key: NCBI GeneID
-#        value: A set of GO IDs associated with that gene
-ns2assoc = objanno.get_ns2assc()
-
-for nspc, id2gos in ns2assoc.items():
-    print("{NS} {N:,} annotated human genes".format(NS=nspc, N=len(id2gos)))
-
-print(len(GeneID2nt_hum))
-
 from goatools.goea.go_enrichment_ns import GOEnrichmentStudyNS
+import os
 
-goeaobj = GOEnrichmentStudyNS(
-    GeneID2nt_hum.keys(),  # List of mouse protein-coding genes
-    ns2assoc,  # geneid/GO associations
-    obodag,  # Ontologies
-    propagate_counts=False,
-    alpha=0.05,  # default significance cut-off
-    methods=['fdr_bh'])  # defult multipletest correction method
+def GO_enrichment(inputFile, project):
+    GeneID2nt_hum = genes_NCBI_9606_ProteinCoding.GENEID2NT
 
-with open("genes.csv", 'r') as gene_symbols:
+    obo_fname = download_go_basic_obo()
+
+    fin_gene2go = download_ncbi_associations()
+
+    obodag = GODag("go-basic.obo")
+
+    # Read NCBI's gene2go. Store annotations in a list of namedtuples
+    objanno = Gene2GoReader(fin_gene2go, taxids=[9606])
+
+    # Get namespace2association where:
+    #    namespace is:
+    #        BP: biological_process
+    #        MF: molecular_function
+    #        CC: cellular_component
+    #    assocation is a dict:
+    #        key: NCBI GeneID
+    #        value: A set of GO IDs associated with that gene
+    ns2assoc = objanno.get_ns2assc()
+
+    for nspc, id2gos in ns2assoc.items():
+        print("{NS} {N:,} annotated human genes".format(NS=nspc, N=len(id2gos)))
+
+    print(len(GeneID2nt_hum))
+
+
+
+    goeaobj = GOEnrichmentStudyNS(
+        GeneID2nt_hum.keys(),  # List of human protein-coding genes
+        ns2assoc,  # geneid/GO associations
+        obodag,  # Ontologies
+        propagate_counts=False,
+        alpha=0.05,  # default significance cut-off
+        methods=['fdr_bh'])  # defult multipletest correction method
+
     geneid2symbol = {}
-    read_genes = csv.reader(gene_symbols)
-    for line in read_genes:
-        gene_id = KEGG_Pathways.symbol2entrezid(line[0])
-        geneid2symbol[gene_id] = line[0]
-gene_symbols.close()
+    with open(inputFile, 'r') as infile:
+        input_genes = csv.reader(infile)
+        for line in input_genes:
+            geneid = line[0]
+            symbol = line[1]
+            if geneid:
+                geneid2symbol[int(geneid)] = symbol
 
-with open("geneids.csv", 'w') as outfile:
-    out_genes = csv.writer(outfile)
-    for gene in geneid2symbol:
-        out_genes.writerow(gene)
-outfile.close()
-
-geneids_study = geneid2symbol.keys()
-goea_results_all = goeaobj.run_study(geneids_study)
-goea_results_sig = [r for r in goea_results_all if r.p_fdr_bh < 0.05]
+    infile.close()
+    print(len(geneid))
 
 
-goeaobj.wr_xlsx("go_enrichment.xlsx", goea_results_sig)
-goeaobj.wr_txt("go_enrichment.txt", goea_results_sig)
+    geneids_study = geneid2symbol.keys()
+    goea_results_all = goeaobj.run_study(geneids_study)
+    goea_results_sig = [r for r in goea_results_all if r.p_fdr_bh < 0.05]
+
+    import collections as cx
+    ctr = cx.Counter([r.NS for r in goea_results_sig])
+    print('Significant results[{TOTAL}] = {BP} BP + {MF} MF + {CC} CC'.format(
+        TOTAL=len(goea_results_sig),
+        BP=ctr['BP'],  # biological_process
+        MF=ctr['MF'],  # molecular_function
+        CC=ctr['CC'])) # cellular_component
+
+    goeaobj.wr_xlsx("go_enrichment" + project + ".csv", goea_results_sig)
+    goeaobj.wr_txt("go_enrichment" + project + ".txt", goea_results_sig)
+
+GO_enrichment("geneids_GSE156544.csv", "GSE156544")
