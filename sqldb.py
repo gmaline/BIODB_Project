40c698057509b09491a70fa11c5c52899d9d1f2e
gmaline <66379163+gmaline@users.noreply.github.com>
2021-05-03 00:02:41 -0500

    Updates to the DDL and DML creation.

--- sqldb.py
+++ sqldb.py
@@ -119,10 +119,12 @@ def format_insert_gene(inFileName, OutFileName):
             if len(symbol) > 0:
                 # Assign null if longer gene name is absent
                 if len(genes[symbol]) == 0:
-                    outFile.write("\nINSERT INTO gene (gene_symbol, gene_name) VALUES (\""
+                    outFile.write("\nINSERT INTO gene (gene_symbol, gene_name)"
+                                  + "VALUES (\""
                                   + symbol + "\", " + "null" + ");")
                 else:
-                    outFile.write("\nINSERT INTO gene (gene_symbol, gene_name) VALUES (\""
+                    outFile.write("\nINSERT INTO gene (gene_symbol, gene_name)"
+                                  + " VALUES (\""
                                   + symbol + "\", \"" + genes[symbol] + "\");")
 
     outFile.close()
@@ -145,8 +147,8 @@ def format_insert_expression(inFileName, outFileName, project_id):
                 direction = line[2]
                 outFile.write("\nINSERT INTO expression (gene_symbol, "
                               + "project_id, sample_id, direction) VALUES (\""
-                              + gene_symbol + "\", \"" + project_id + "\", \"" + sample_id
-                              + "\", \"" + direction + "\");")
+                              + gene_symbol + "\", \"" + project_id + "\", \""
+                              + sample_id + "\", \"" + direction + "\");")
     outFile.close()
 
 # Name: format_insert_KEGG
@@ -166,7 +168,8 @@ def format_insert_KEGG(inFileName, outFileName):
             pathways[pathway_id] = pathway_name
         for pathway_id in pathways.keys():
             pathway_name = pathways[pathway_id]
-            outFile.write("\nINSERT INTO KEGG_Pathway (pathway_id, pathway_name) VALUES (\""
+            outFile.write("\nINSERT INTO KEGG_Pathway (pathway_id,"
+                          + " pathway_name) VALUES (\""
                           + pathway_id + "\", \"" + pathway_name + "\");")
     outFile.close()
 
@@ -186,8 +189,9 @@ def format_insert_FunctionsIn(inFileName, outFileName):
             pathway_name = line[2]
             pairs[gene_symbol + " " + pathway_name] = [gene_symbol, pathway_name]
         for pair in pairs.keys():
-            outFile.write("\nINSERT INTO Functions_In (gene_symbol, pathway_name) VALUES (\""
-                          + pairs[pair][0] + "\", \"" + pairs[pair][1] + "\");")
+            outFile.write("\nINSERT INTO Functions_In (gene_symbol, "
+                          + "pathway_name) VALUES (\"" + pairs[pair][0]
+                          + "\", \"" + pairs[pair][1] + "\");")
     outFile.close()
 
 # Name: format_insert_GO
@@ -207,8 +211,8 @@ def format_insert_GO(inFileName, outFileName):
             terms[go_id] = go_term
         for go_id in terms.keys():
             go_term = terms[go_id]
-            outFile.write("\nINSERT INTO Gene_Ontology (go_id, go_term) VALUES (\""
-                          + go_id + "\", \"" + go_term + "\");")
+            outFile.write("\nINSERT INTO Gene_Ontology (go_id, go_term) VALUES"
+                          + " (\"" + go_id + "\", \"" + go_term + "\");")
     outFile.close()
 
 # Name: format_insert_DescribedBy
@@ -224,8 +228,9 @@ def format_insert_DescribedBy(inFileName, outFileName, project):
         reader = csv.reader(inFile)
         for line in reader:
             go_id = line[0]
-            outFile.write("\nINSERT INTO Described_By (go_id, project_id) VALUES (\""
-                          + go_id + "\", \"" + project + "\");")
+            outFile.write("\nINSERT INTO Described_By (go_id, project_id) "
+                          + "VALUES (\"" + go_id + "\", \"" + project
+                          + "\");")
     outFile.close()
 
 # Name: format_insert_project
@@ -244,8 +249,11 @@ def format_insert_project(inFileName, outFileName):
             proj_date = line[2]
             taxid = line[3]
             tissue = line[4]
-            outFile.write("\nINSERT INTO project (project_id, authors, proj_date, taxid, tissue) VALUES (\""
-                          + project_id + "\", \"" + authors + "\", \"" + proj_date + "\", \"" + taxid + "\", \"" + tissue + "\");")
+            outFile.write("\nINSERT INTO project (project_id, authors,"
+                          + " proj_date, taxid, tissue) VALUES (\""
+                          + project_id + "\", \"" + authors + "\", \""
+                          + proj_date + "\", \"" + taxid + "\", \"" + tissue
+                          + "\");")
     outFile.close()
 
 
@@ -262,16 +270,30 @@ create_FunctionsIn()
 create_project()
 
 # Creating the sql insert files
-format_insert_project("project_info_data.csv", "insert_project.sql")
+format_insert_project("Data/project_info_data.csv",
+                      "DDL_and_DML/insert_project.sql")
 
-format_insert_gene("expression_data_all.csv", "insert_gene_all.sql")
-format_insert_KEGG("kegg_pathway_all.csv", "insert_kegg_all.sql")
-format_insert_GO("go_enrichmentall.csv", "insert_GO_all.sql")
+format_insert_gene("Data/expression_data_all.csv",
+                   "DDL_and_DML/insert_gene_all.sql")
+format_insert_KEGG("Data/kegg_pathway_all.csv",
+                   "DDL_and_DML/insert_kegg_all.sql")
+format_insert_GO("Data/go_enrichmentall.csv",
+                 "DDL_and_DML/insert_GO_all.sql")
 
-format_insert_expression("expression_data_GSE156544.csv", "insert_expression_GSE156544.sql", "GSE156544")
-format_insert_FunctionsIn("kegg_pathway_GSE156544.csv", "insert_functionsin_GSE156544.sql")
-format_insert_DescribedBy("go_enrichmentGSE156544.csv", "insert_describes_GSE156544.sql", "GSE156544")
+format_insert_expression("Data/expression_data_GSE156544.csv",
+                         "DDL_and_DML/insert_expression_GSE156544.sql",
+                         "GSE156544")
+format_insert_FunctionsIn("Data/kegg_pathway_GSE156544.csv",
+                          "DDL_and_DML/insert_functionsin_GSE156544.sql")
+format_insert_DescribedBy("Data/go_enrichmentGSE156544.csv",
+                          "DDL_and_DML/insert_describes_GSE156544.sql",
+                          "GSE156544")
 
-format_insert_expression("expression_data_BioProjectPRJNA634489.csv", "insert_expression_BioProjectPRJNA634489.sql", "BioProjectPRJNA634489")
-format_insert_FunctionsIn("kegg_pathway_BioProjectPRJNA634489.csv", "insert_functionsin_BioProjectPRJNA634489.sql")
-format_insert_DescribedBy("go_enrichmentBioProjectPRJNA634489.csv", "insert_describes_BioProjectPRJNA634489.sql", "BioProjectPRJNA634489")
+format_insert_expression("Data/expression_data_BioProjectPRJNA634489.csv",
+                         "DDL_and_DML/insert_expression_BioProjectPRJNA634489.sql",
+                         "BioProjectPRJNA634489")
+format_insert_FunctionsIn("Data/kegg_pathway_BioProjectPRJNA634489.csv",
+                          "DDL_and_DML/insert_functionsin_BioProjectPRJNA634489.sql")
+format_insert_DescribedBy("Data/go_enrichmentBioProjectPRJNA634489.csv",
+                          "DDL_and_DML/insert_describes_BioProjectPRJNA634489.sql",
+                          "BioProjectPRJNA634489")
